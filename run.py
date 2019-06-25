548d72dcce6fe05b6d99f340d28fadc44b8c9f0b
nozawa <taktaktak0812@gmail.com>
2019-06-25 18:41:14 +0900

    CHANGE ID

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('FePtkG7zKRo1Ln6EetEhTlokYX++0Cda52bbe91/SK11Y09EeYNUxLmSGKJgQVF5FPff/iANOE1UpBhMHvWF4k4VUaupEcv4rBpi9zI4EYCEMZ1l7EJmXAJ0M+sKebGf38Uav3Biipend0ZSGmxYHAdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('6AcDr4HJnpA21YxGDX65w5PxyU4UAfFHzZzi84uogbsBmfjE0vKQQ8Iw5ICNdFk8FPff/iANOE1UpBhMHvWF4k4VUaupEcv4rBpi9zI4EYDM12RWxs2CO5Z8wXLP7rXKD2WVdwhR0e7hn1XIRXYSpQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('cd4e2c8ca25810a3f250df277ee6527f')
 
 

acf218877b75ebcead1d32b6bd84b05f28951656
nozawa <taktaktak0812@gmail.com>
2019-06-25 22:59:06 +0900

    bug fix

--- run.py
+++ run.py
@@ -43,7 +43,7 @@ def handle_message(event):
 def echo(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=[event.message.text, event.source.user_id]))
+        TextSendMessage(text=event.message.text))
 
 def get_user_id(event):
     line_bot_api.reply_message(

cf7e2a0d6eb1e771a6559b2de5e4e1f854b306c3
nozawa <taktaktak0812@gmail.com>
2019-07-01 17:43:35 +0900

    remove config.py from tracking file

--- config.py
+++
@@ -1,5 +0,0 @@
-def getAccessToken():
-    return '6AcDr4HJnpA21YxGDX65w5PxyU4UAfFHzZzi84uogbsBmfjE0vKQQ8Iw5ICNdFk8FPff/iANOE1UpBhMHvWF4k4VUaupEcv4rBpi9zI4EYDM12RWxs2CO5Z8wXLP7rXKD2WVdwhR0e7hn1XIRXYSpQdB04t89/1O/w1cDnyilFU='
-
-def getChannelSecret():
-    return 'cd4e2c8ca25810a3f250df277ee6527f'
